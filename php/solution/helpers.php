b28307addb80ec9146f111fa548a671dd93967e1
unknown <erikjairsilva@gmail.com>
2021-08-20 12:32:50 -0500

    Resultados prueba

--- php/solution/helpers.php
+++ php/solution/helpers.php
@@ -1,13 +1,20 @@
 <?php
 
-if(!function_exists('sumOrMultiply')){
- function sumOrMultiply(){
+$_ACCESSIBLE_GLOBAL_INT = 0;
 
+if(!function_exists('sumOrMultiply')){
+ function sumOrMultiply($arr){
+     if(sizeof($arr) >= 5){
+         return array_sum($arr);
+     }else{
+         return array_product($arr);
+     }
  }
 }
 
 if(!function_exists('setAccessibleGlobalInt')){
- function setAccesibleGlobalInt(){
-
+ function setAccesibleGlobalInt($value){
+     global $_ACCESSIBLE_GLOBAL_INT;
+     return $_ACCESSIBLE_GLOBAL_INT = $value;
  }
 }
